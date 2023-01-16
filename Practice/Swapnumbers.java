81a8ca0ed62d1bb97dd50372d0f1671d0a271206
vedant027 <vedant.thorat27@gmail.com>
2023-01-16 22:07:58 +0530

    16th Jan

---
+++ Practice/Swapnumbers.java
@@ -0,0 +1,12 @@
+public class Swapnumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int temp = a;
+        a= b;
+        b = temp;
+
+        System.out.println(a + " " + b);
+    }
+}
