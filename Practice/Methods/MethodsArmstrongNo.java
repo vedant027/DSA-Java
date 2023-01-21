4c3e25f4d2e6c94f9830303290ce10a3068b7aa5
vedant027 <vedant.thorat27@gmail.com>
2023-01-21 23:13:43 +0530

    21st Jan

---
+++ Practice/Methods/MethodsArmstrongNo.java
@@ -0,0 +1,32 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class MethodsArmstrongNo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        for(int i = 100; i < 1000; i++){
+            if(isArmstrong(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    static boolean isArmstrong(int n){
+        int original = n;
+        int sum = 0;
+
+        while(n > 0) {
+            int rem = n % 10;
+            n = n / 10;
+            sum = sum + rem*rem*rem;
+        }
+        if (sum == original){
+            return true;
+        }
+        return false;
+    }
+
+
+}
---
+++ Practice/Methods/MethodsPrimeNo.java
@@ -0,0 +1,30 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class MethodsPrimeNo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        boolean ans = isPrime(n);
+        System.out.println(ans);
+
+    }
+
+    static boolean isPrime(int n){
+        if(n <= 1){
+            return false;
+        }
+        int c = 2;
+        while(c*c <= n){
+            if(n % c == 0){
+                return false;
+            }
+            c++;
+        }
+        if (c * c > n) {
+            return true;
+        }
+        return false;
+    }
+}
