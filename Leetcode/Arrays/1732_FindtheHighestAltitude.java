49547011d70e03258ffd2ae90cd922281b52ae09
vedant027 <vedant.thorat27@gmail.com>
2023-02-08 20:16:31 +0530

    8th Feb

---
+++ Leetcode/Arrays/1732_FindtheHighestAltitude.java
@@ -0,0 +1,15 @@
+public class FindtheHighestAltitude {
+    public static void main(String[] args) {
+        class Solution {
+            public int largestAltitude(int[] gain) {
+                int max = 0;
+                int current = 0;
+                for(int i=0; i<gain.length; i++){
+                    current+=gain[i];
+                    max=Math.max(current,max);
+                }
+                return max;
+            }
+        }
+    }
+}
