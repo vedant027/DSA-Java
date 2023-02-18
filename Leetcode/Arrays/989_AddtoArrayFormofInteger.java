a9ae2165b73c33db88a5a2e605bab35499a11235
vedant027 <vedant.thorat27@gmail.com>
2023-02-18 20:06:00 +0530

    18th Feb

---
+++ Leetcode/Arrays/989_AddtoArrayFormofInteger.java
@@ -0,0 +1,24 @@
+public class AddtoArrayFormofInteger {
+    public static void main(String[] args) {
+        class Solution {
+            public List<Integer> addToArrayForm(int[] num, int k) {
+                int n = num.length;
+                int i = n-1;
+                List<Integer> ans = new ArrayList();
+                while (i >= 0 || k > 0){
+                    if (i >= 0){
+                        k = k + num[i];
+                        ans.add(k % 10);
+                        k = k / 10;
+                    }else {
+                        ans.add(k % 10);
+                        k = k / 10;
+                    }
+                    i--;
+                }
+                Collections.reverse(ans);
+                return ans;
+            }
+        }
+    }
+}
