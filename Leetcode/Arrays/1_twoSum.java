8fd1549a3b3edd03294efede5859a2720af8a61e
vedant027 <vedant.thorat27@gmail.com>
2023-02-21 23:41:10 +0530

    21th Feb

--- Leetcode/Arrays/1_twoSum.java
+++ Leetcode/Arrays/1_twoSum.java
@@ -3,6 +3,8 @@ public class twoSum {
         class Solution {
             public int[] twoSum(int[] nums, int target) {
                 int n = nums.length;
+
+                // keeping i constant for iteration and checking for every value of j
                 for(int i=0; i<n; i++){
                     for (int j = i + 1; j < nums.length; j++) {
                         if (nums[j] == target - nums[i]) {
