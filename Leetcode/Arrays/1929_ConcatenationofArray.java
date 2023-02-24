cb7b880d586f3ba2741e9636c06c9b7214829af7
vedant027 <vedant.thorat27@gmail.com>
2023-02-24 22:34:14 +0530

    24th Feb

--- Leetcode/Arrays/1929_ConcatenationofArray.java
+++ Leetcode/Arrays/1929_ConcatenationofArray.java
@@ -4,6 +4,7 @@ public class ConcatenationofArray {
             public int[] getConcatenation(int[] nums) {
                 int[] ans = new int[2 * nums.length];
 
+                // Making a new array ans and doubling the size of input array with same input values
                 for(int i=0; i < nums.length; i++){
                     ans[i] = ans[i + nums.length] = nums[i];
                 }
