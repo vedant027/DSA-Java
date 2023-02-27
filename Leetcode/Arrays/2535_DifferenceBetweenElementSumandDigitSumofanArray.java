13f2d782821d7c215b04ef544f10f8c140d1d7a0
vedant027 <vedant.thorat27@gmail.com>
2023-02-27 23:19:51 +0530

    27th Feb

--- Leetcode/Arrays/2535_DifferenceBetweenElementSumandDigitSumofanArray.java
+++ Leetcode/Arrays/2535_DifferenceBetweenElementSumandDigitSumofanArray.java
@@ -4,8 +4,12 @@ public class DifferenceBetweenElementSumandDigitSumofanArray {
             public int differenceOfSum(int[] nums) {
                 int elementsum=0, digitsum=0;
                 int rem=0;
+
+                    // Calculating the Element Sum
                 for(int i=0; i<nums.length; i++){
                     elementsum += nums[i];
+
+                    // Calculating the Digit Sum
                     if(nums[i] < 10)
                         digitsum += nums[i];
                     else{
@@ -16,6 +20,8 @@ public class DifferenceBetweenElementSumandDigitSumofanArray {
                         }
                     }
                 }
+
+                // Returning Difference between Element Sum and Digit Sum
                 return Math.abs(elementsum - digitsum);
             }
         }
