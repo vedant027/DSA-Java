a9f84c7afa151a2f2a207bb6de7f6ae2f2958c39
vedant027 <vedant.thorat27@gmail.com>
2023-03-04 23:49:38 +0530

    4th March

--- Leetcode/Arrays/867_TransposeMatrix.java
+++ Leetcode/Arrays/867_TransposeMatrix.java
@@ -6,6 +6,7 @@ public class TransposeMatrix {
                 int[][] ans = new int[C][R];
                 for (int r = 0; r < R; ++r)
                     for (int c = 0; c < C; ++c) {
+                        // Interchanging the original matrix row and column for the transpose matrix
                         ans[c][r] = matrix[r][c];
                     }
                 return ans;
