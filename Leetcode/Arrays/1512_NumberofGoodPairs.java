36a706641aa46d43703ee0fbb5fa8f39c0d6a28c
vedant027 <vedant.thorat27@gmail.com>
2023-03-07 23:07:28 +0530

    7th March

--- Leetcode/Arrays/1512_NumberofGoodPairs.java
+++ Leetcode/Arrays/1512_NumberofGoodPairs.java
@@ -5,7 +5,9 @@ public class NumberofGoodPairs {
                 int ans=0;
                 for(int i=0;i<nums.length;i++){
                     for(int j=i+1;j<nums.length;j++){
+                        // Checking if there is any similar element in the array
                         if(nums[i]==nums[j]){
+                            // Incrementing the count of good pairs in ans
                             ans++;
                         }
                     }
