e93dd69e158df55b6ba0452caaecc8c2bc7d8f1b
vedant027 <vedant.thorat27@gmail.com>
2023-03-12 23:14:09 +0530

    12th March

--- Leetcode/Math/2235_Add2Integers.java
+++ Leetcode/Math/2235_Add2Integers.java
@@ -3,6 +3,7 @@ public class Add2Integers {
         class Solution {
             public int sum(int num1, int num2) {
                 for(int i= -200; i <= 200; i++){
+                    // Adding two integers
                     if(num1 + num2 == i){
                         return i;
                     }
