a5835501885df1b8624a61ff2b3a37093fa2ffe4
vedant027 <vedant.thorat27@gmail.com>
2023-03-13 22:32:32 +0530

    13th March

--- Leetcode/Arrays/1920_BuildArrayfromPermutation.java
+++ Leetcode/Arrays/1920_BuildArrayfromPermutation.java
@@ -2,8 +2,9 @@ class BuildArrayfromPermutation {
     public static void main(String[] args) {
         class Solution {
             public int[] buildArray(int[] nums) {
-                int[] ans = new int[nums.length];
-                for(int i = 0; i < nums.length; i++){
+                int n = nums.length;
+                int[] ans = new int[n];
+                for(int i = 0; i < n; i++){
                     ans[i] = nums[nums[i]];
                 }
                 return ans;
