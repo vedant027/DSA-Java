a8e5d1ae724ec1396ea98b44c9d16ba9d3c1f376
vedant027 <vedant.thorat27@gmail.com>
2023-03-15 23:46:13 +0530

    15th March

--- Leetcode/Arrays/1480_Runningsumof1DArray.java
+++ Leetcode/Arrays/1480_Runningsumof1DArray.java
@@ -4,8 +4,9 @@ public class Runningsumof1DArray {
 
         class Solution {
             public int[] runningSum(int[] nums) {
+                int n = nums.length;
                 int sum = 0;
-                for(int i = 0; i < nums.length; i++){
+                for(int i = 0; i < n; i++){
                     // Adding sum of previous numbers with next element
                     sum = sum + nums[i];
                     nums[i] = sum;
