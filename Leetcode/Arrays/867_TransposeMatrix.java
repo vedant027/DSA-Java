889539f1538e2878db5d8bd3effa45f2314e1b41
vedant027 <vedant.thorat27@gmail.com>
2023-03-16 23:28:38 +0530

    16th March

--- Leetcode/Arrays/867_TransposeMatrix.java
+++ Leetcode/Arrays/867_TransposeMatrix.java
@@ -3,6 +3,7 @@ public class TransposeMatrix {
         class Solution {
             public int[][] transpose(int[][] matrix) {
                 int R = matrix.length, C = matrix[0].length;
+                // Creating the transpose matrix
                 int[][] ans = new int[C][R];
                 for (int r = 0; r < R; ++r)
                     for (int c = 0; c < C; ++c) {
