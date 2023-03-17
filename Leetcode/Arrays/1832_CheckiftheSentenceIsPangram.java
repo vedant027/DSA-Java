240dd17013d007aaace59bc3e8c399e884cea456
vedant027 <vedant.thorat27@gmail.com>
2023-03-17 23:04:28 +0530

    17th March

--- Leetcode/Arrays/1832_CheckiftheSentenceIsPangram.java
+++ Leetcode/Arrays/1832_CheckiftheSentenceIsPangram.java
@@ -2,6 +2,7 @@ public class CheckiftheSentenceIsPangram {
     public static void main(String[] args) {
         class Solution {
             public boolean checkIfPangram(String sentence) {
+//  Checking every letter of sentence for matching with alphabets
                 for (int i = 0; i < 26; ++i) {
                     char currChar = (char)('a' + i);
 
