bbf4f19aaf3b60a48f4c15cf6c9112f7cb4e02e3
vedant027 <vedant.thorat27@gmail.com>
2023-03-19 20:57:05 +0530

    19th March

--- Leetcode/Arrays/1470_ShuffletheArray.java
+++ Leetcode/Arrays/1470_ShuffletheArray.java
@@ -3,8 +3,11 @@ public class ShuffletheArray {
         class Solution {
             public int[] shuffle(int[] nums, int n) {
                 int[] result = new int[2 * n];
+
                 for(int i = 0; i < n; i++){
+                    //  For even positions in the result array
                     result[2 * i] = nums[i];
+                    //  For odd positions in the result array
                     result[2 * i + 1] = nums[n + i];
                 }
                 return result;
