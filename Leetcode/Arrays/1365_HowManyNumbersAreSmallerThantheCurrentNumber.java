ad65d0a4b6a2b4165beb6327cc3ac1a8bfafedc9
vedant027 <vedant.thorat27@gmail.com>
2023-03-21 23:23:32 +0530

    21st March

--- Leetcode/Arrays/1365_HowManyNumbersAreSmallerThantheCurrentNumber.java
+++ Leetcode/Arrays/1365_HowManyNumbersAreSmallerThantheCurrentNumber.java
@@ -4,6 +4,7 @@ public class HowManyNumbersAreSmallerThantheCurrentNumber {
             public int[] smallerNumbersThanCurrent(int[] nums) {
                 int n = nums.length;
                 int[] arr = new int[n];
+                // Creating an array for counting smaller numbers
                 for(int i=0; i<n; i++){
                     int count = 0;
                     for(int j=0; j<n; j++){
