5b832226681ecb9cf01a3ba499932b8eebb1b4bc
vedant027 <vedant.thorat27@gmail.com>
2023-03-29 23:08:48 +0530

    29th March

--- Leetcode/Arrays/724_FindPivotIndex.java
+++ Leetcode/Arrays/724_FindPivotIndex.java
@@ -2,17 +2,17 @@ public class FindPivotIndex {
     public static void main(String[] args) {
         class Solution {
             public int pivotIndex(int[] nums) {
-                int total_sum=0;
+                int totalSum=0;
+                int leftSum=0;
+
                 // Creating loop for calculating total sum
                 for(int i=0; i<nums.length; i++){
-                    total_sum += nums[i];
+                    totalSum += nums[i];
                 }
-
                 // Creating loop for calculating the left sum
-                int left_sum=0;
                 for(int i=0; i<nums.length; i++){
-                    if(i != 0) left_sum += nums[i-1];
-                    if(total_sum - left_sum - nums[i] == left_sum){
+                    if(i != 0) leftSum += nums[i-1];
+                    if(totalSum - leftSum - nums[i] == leftSum){
                         return i;
                     }
                 }
