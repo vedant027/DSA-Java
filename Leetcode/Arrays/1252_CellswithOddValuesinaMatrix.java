118fb7b02aaa93fa6a4bf75d986bec2d771e1a2b
vedant027 <vedant.thorat27@gmail.com>
2023-04-02 16:48:56 +0530

    2nd April

--- Leetcode/Arrays/1252_CellswithOddValuesinaMatrix.java
+++ Leetcode/Arrays/1252_CellswithOddValuesinaMatrix.java
@@ -9,15 +9,15 @@ public class CellswithOddValuesinaMatrix {
                     rows[a[0]]++;
                     cols[a[1]]++;
                 }
-                int res=0;
+                int count=0;
                 for(int i=0;i<m;i++){
                     for(int j=0;j<n;j++){
                         if((rows[i]+cols[j])%2 != 0){
-                            res++;
+                            count++;
                         }
                     }
                 }
-                return res;
+                return count;
             }
         }
     }
