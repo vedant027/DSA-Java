5caec0664d844677c5817a5526c57aa63ed0dcbc
vedant027 <vedant.thorat27@gmail.com>
2023-04-04 12:41:42 +0530

    4th April

---
+++ Leetcode/Arrays/34_FindFirstandLastPositionofElementinSortedArray.java
@@ -0,0 +1,47 @@
+public class FindFirstandLastPositionofElementinSortedArray {
+    class Solution {
+        public int[] searchRange(int[] nums, int target) {
+            int[] result = new int[2];
+            result[0] = startingIndex(nums, target);
+            result[1] = endingIndex(nums, target);
+            return result;
+        }
+
+        public int startingIndex(int[] nums, int target) {
+            int index = -1;
+
+            int start = 0;
+            int end = nums.length-1;
+
+            while(start <= end){
+                int midpoint = start + (end - start)/2;
+                if(nums[midpoint] >= target){
+                    end = midpoint - 1;
+                } else {
+                    start = midpoint + 1;
+                }
+                if (nums[midpoint] == target) index = midpoint;
+            }
+            return index;
+        }
+
+        public int endingIndex(int[] nums, int target) {
+            int index = -1;
+
+            int start = 0;
+            int end = nums.length-1;
+
+            while(start <= end){
+                int midpoint = start + (end - start)/2;
+                if(nums[midpoint] <= target){
+                    start = midpoint + 1;
+                } else {
+                    end = midpoint - 1;
+                }
+                if (nums[midpoint] == target) index = midpoint;
+
+            }
+            return index;
+        }
+    }
+}
