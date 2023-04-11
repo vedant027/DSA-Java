1cc4b8b3aceb9f3df70d5e46c35e0f9b5ae2cc00
vedant027 <vedant.thorat27@gmail.com>
2023-04-11 17:53:28 +0530

    11th April

---
+++ Leetcode/Math/412_FizzBuzz.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        class Solution {
+            public List<String> fizzBuzz(int n) {
+                List<String> answer = new ArrayList<String>();
+
+                for(int i=1; i<=n; i++){
+                    if(i % 3 == 0 && i % 5 == 0){
+                        answer.add("FizzBuzz");
+                    }
+                    else if(i % 3 == 0){
+                        answer.add("Fizz");
+                    }
+                    else if(i % 5 == 0){
+                        answer.add("Buzz");
+                    }
+                    else{
+                        answer.add(String.valueOf(i));
+                    }
+                }
+                return answer;
+            }
+        }
+    }
+}
