155c367c5bb66cfa85e3674916f9fb7570813455
vedant027 <vedant.thorat27@gmail.com>
2023-04-14 21:04:09 +0530

    14th April

---
+++ Leetcode/String/771_JewelsandStones.java
@@ -0,0 +1,18 @@
+public class JewelsandStones {
+    public static void main(String[] args) {
+        class Solution {
+            public int numJewelsInStones(String jewels, String stones) {
+                int count = 0;
+
+                for(int i=0; i<stones.length(); i++){
+                    for(int j=0; j<jewels.length(); j++){
+                        if(jewels.charAt(j) == stones.charAt(i)){
+                            count++;
+                        }
+                    }
+                }
+                return count++;
+            }
+        }
+    }
+}
