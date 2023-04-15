e2f4b164d3241491b59aa587ec7aef338863ac77
vedant027 <vedant.thorat27@gmail.com>
2023-04-15 21:17:12 +0530

    15th April

---
+++ Leetcode/String/709_ToLowerCase.java
@@ -0,0 +1,11 @@
+public class ToLowerCase {
+    public static void main(String[] args) {
+        class Solution {
+            public String toLowerCase(String s) {
+
+                String words = s.toLowerCase();
+                return words;
+            }
+        }
+    }
+}
