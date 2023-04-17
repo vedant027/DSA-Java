4ab8aa5933c6c3b8dab2f45a6da7d9088bf30a11
vedant027 <vedant.thorat27@gmail.com>
2023-04-17 20:31:17 +0530

    17th April

---
+++ Leetcode/Math/258_AddDigits.java
@@ -0,0 +1,19 @@
+public class AddDigits {
+    public static void main(String[] args) {
+        class Solution {
+            public int addDigits(int num) {
+
+                if(num == 0){
+                    return 0;
+                }
+                else if(num % 9 == 0){
+                    return 9;
+                }
+                else{
+                    return num % 9;
+                }
+
+            }
+        }
+    }
+}
