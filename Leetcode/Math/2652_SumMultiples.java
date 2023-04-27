bf8590ad4251e2dd1e95e4ec9433b009ef98b02f
vedant027 <vedant.thorat27@gmail.com>
2023-04-27 23:16:02 +0530

    27th April

---
+++ Leetcode/Math/2652_SumMultiples.java
@@ -0,0 +1,17 @@
+public class SumMultiples {
+    public static void main(String[] args) {
+        class Solution {
+            public int sumOfMultiples(int n) {
+                int sum=0;
+                for(int i=3;i<=n;i++)
+                {
+                    if(i%3==0 || i%5==0 || i%7==0)
+                    {
+                        sum+=i;
+                    }
+                }
+                return sum;
+            }
+        }
+    }
+}
