935d94b70265106a14feb929bf82eb41a18e1cb8
vedant027 <vedant.thorat27@gmail.com>
2023-05-02 16:06:24 +0530

    2nd May

---
+++ Leetcode/Math/2427_NumberofCommonFactors.java
@@ -0,0 +1,17 @@
+public class NumberofCommonFactors {
+    public static void main(String[] args) {
+        class Solution {
+            public int commonFactors(int a, int b) {
+                int min = Math.min(a, b);
+                int count = 0;
+
+                for(int i=1; i<=min; i++){
+                    if((a%i ==0) && (b%i ==0)){
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+    }
+}
